366e8fe60ff38cb5ae85298fc08d98c8303b0f2c
tomeko74 <34099068+tomeko74@users.noreply.github.com>
2017-11-29 22:22:55 +0100

    po trzeciej lekcji

--- run.py
+++ run.py
@@ -2,10 +2,14 @@ import pygame, sys
 
 #print(pygame.__version__)
 
+max_tps = 70.0
+
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 box = pygame.Rect(10, 10, 50, 50)
 clock = pygame.time.Clock()
+delta = 0.0
+
 
 while True:
     # Handle events
@@ -15,17 +19,21 @@ while True:
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit(0)
 
+    # Ticking
+    delta += clock.tick()/1000.0
+    while delta > 1 / max_tps:
+        delta -= 1 / max_tps
 
-    # Checking inputs
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_d]:
-        box.x += 1
-    if keys[pygame.K_s]:
-        box.y += 1
-    if keys[pygame.K_w]:
-        box.y -= 1
-    if keys[pygame.K_a]:
-        box.x -= 1
+        # Checking inputs
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            box.x += 1
+        if keys[pygame.K_s]:
+            box.y += 1
+        if keys[pygame.K_w]:
+            box.y -= 1
+        if keys[pygame.K_a]:
+            box.x -= 1
 
 
     # Drawing
