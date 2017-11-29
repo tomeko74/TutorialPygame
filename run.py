999b7639f676390d1fd2b0b4f265ce6502bd544a
tomeko74 <34099068+tomeko74@users.noreply.github.com>
2017-11-29 18:08:06 +0100

    po drugiej lekcji

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
-import pygame
+import pygame, sys
 
-print(pygame.__version__)
+#print(pygame.__version__)
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+box = pygame.Rect(10, 10, 50, 50)
+
+while True:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            sys.exit(0)
+
+
+    # Checking inputs
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        box.x += 1
+    if keys[pygame.K_s]:
+        box.y += 1
+    if keys[pygame.K_w]:
+        box.y -= 1
+    if keys[pygame.K_a]:
+        box.x -= 1
+
+
+    # Drawing
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (0, 150, 255), box)
+    pygame.display.flip()
