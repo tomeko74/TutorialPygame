accc92447541a52b2c52b943e2d751e81455d46f
tomeko74 <34099068+tomeko74@users.noreply.github.com>
2017-11-30 10:16:27 +0100

    po czwartej lekcji

--- run.py
+++ run.py
@@ -1,42 +1,50 @@
 import pygame, sys
+from rocket import Rocket
 
-#print(pygame.__version__)
+class Game(object):
 
-max_tps = 70.0
+    def __init__(self):
+        # Config
+        self.tps_max = 100.0
 
-pygame.init()
-screen = pygame.display.set_mode((1280,720))
-box = pygame.Rect(10, 10, 50, 50)
-clock = pygame.time.Clock()
-delta = 0.0
+        # Init
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.tps_clock = pygame.time.Clock()
+        self.tps_delta = 0.0
+
+        self.player = Rocket(self)
+
+        while True:
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+
+            # Ticking
+            self.tps_delta += self.tps_clock.tick()/1000.0
+            while self.tps_delta > 1 / self.tps_max:
+                self.tick()
+                self.tps_delta -= 1 / self.tps_max
+
+            # Drawing
+            self.screen.fill((0, 0, 0))
+            self.draw()
+            pygame.display.flip()
+
+    def tick(self):
+        self.player.tick()
+
+    def draw(self):
+        self.player.draw()
 
 
-while True:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            sys.exit(0)
-
-    # Ticking
-    delta += clock.tick()/1000.0
-    while delta > 1 / max_tps:
-        delta -= 1 / max_tps
-
-        # Checking inputs
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_d]:
-            box.x += 1
-        if keys[pygame.K_s]:
-            box.y += 1
-        if keys[pygame.K_w]:
-            box.y -= 1
-        if keys[pygame.K_a]:
-            box.x -= 1
+if __name__ == "__main__":
+    Game()
 
 
-    # Drawing
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (0, 150, 255), box)
-    pygame.display.flip()
+
+
+
