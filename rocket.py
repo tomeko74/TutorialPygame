104d5d2f5ef3645c193d29b5ead5bb76e68d3af0
tomeko74 <34099068+tomeko74@users.noreply.github.com>
2017-11-30 16:34:45 +0100

    po piątej lekcji

--- rocket.py
+++ rocket.py
@@ -1,13 +1,44 @@
 import pygame
+from pygame.math import Vector2
 
 class Rocket(object):
 
     def __init__(self, game):
         self.game = game
+        self.speed = 1.3
+        self.gravity = 0.5
+
+        size = self.game.screen.get_size()
+
+        self.pos = Vector2(size[0]/2,size[1]/2)
+        self.vel = Vector2(0,0)
+        self.acc = Vector2(0,0)
+
+    def add_force(self, force):
+        self.acc += force
 
     def tick(self):
-        pass
+        # Input
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_w]:
+            self.add_force(Vector2(0, -self.speed))
+        if pressed[pygame.K_s]:
+            self.add_force(Vector2(0, self.speed))
+        if pressed[pygame.K_a]:
+            self.add_force(Vector2(-self.speed, 0))
+        if pressed[pygame.K_d]:
+            self.add_force(Vector2(self.speed, 0))
+
+        # Physics
+        self.vel *= 0.8 # opór powietrza
+        self.vel -= Vector2(0, -self.gravity) # grawitacja
+
+        self.vel += self.acc
+        self.pos += self.vel
+        self.acc *= 0
+
 
     def draw(self):
-        pygame.draw.rect(self.game.screen, (0, 150, 255), pygame.Rect(10, 10, 50, 50))
+        rect = pygame.Rect(self.pos.x, self.pos.y, 20, 50)
+        pygame.draw.rect(self.game.screen, (0, 150, 255), rect)
 
