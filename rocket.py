6776873074e2ec0ce73ad7a540affc2281fdc162
tomeko74 <34099068+tomeko74@users.noreply.github.com>
2017-11-30 21:24:50 +0100

    po szóstej lekcji

--- rocket.py
+++ rocket.py
@@ -39,6 +39,18 @@ class Rocket(object):
 
 
     def draw(self):
-        rect = pygame.Rect(self.pos.x, self.pos.y, 20, 50)
-        pygame.draw.rect(self.game.screen, (0, 150, 255), rect)
+        # Base triangle
+        points = [Vector2(0,-10), Vector2(5,5), Vector2(-5,5)]
+
+        # Rotate points
+        angle = self.vel.angle_to(Vector2(0,1)) # kąt między linią poziomą a prędkością
+        points = [p.rotate(angle) for p in points]
+
+        # Fix y axis
+        points = [Vector2(p.x, p.y*-1) for p in points]
+
+        # Add current position
+        points = [self.pos+p*2 for p in points]
+
+        pygame.draw.polygon(self.game.screen, (0,100,255),points)
 
